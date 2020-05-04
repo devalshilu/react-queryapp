c637ca57e1deb1c5452779b6e93dbb22fa7bba69
Deval Shilu <Deval.Shilu@capita.co.uk>
2020-05-04 20:01:45 +0530

    dynamic sub

--- src/HomePage/StudentList.jsx
+++ src/HomePage/StudentList.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 class StudentList extends React.Component {
 
     render() {
-        const { students } = this.props;
+        const { students, subjects } = this.props;
         return (
             <div className="card border-primary mb-3">
                 < div className="card-header" > Student List</div >
@@ -12,22 +12,23 @@ class StudentList extends React.Component {
                         <thead>
                             <tr>
                                 <th>Student Name</th>
-                                <th>Math</th>
-                                <th>English</th>
-                                <th>Science</th>
-                                <th>Hindi</th>
-                                <th>Computer</th>
+                                {subjects.items && subjects.items.map(function (sub, i) {
+                                    return <th>{sub.name}</th>
+                                })}
                             </tr>
                         </thead>
                         <tbody>
                             {students.items && students.items.map(function (item, i) {
                                 return <tr key={i}>
                                     <td>{item.name}</td>
-                                    <td>{item.result[0].Math}</td>
+                                    {subjects.items && subjects.items.map(function (sub, i) {
+                                        return <th>{item.result[i][sub.name]}</th>
+                                    })}
+                                    {/* <td>{item.result[0].Math}</td>
                                     <td>{item.result[1].English}</td>
                                     <td>{item.result[2].science}</td>
                                     <td>{item.result[3].hindi}</td>
-                                    <td>{item.result[4].Computer}</td>
+                                    <td>{item.result[4].Computer}</td> */}
                                 </tr>
                             })}
                         </tbody>
@@ -40,11 +41,10 @@ class StudentList extends React.Component {
 }
 
 function mapStateToProps(state) {
-    const { students } = state;
+    const { students, subjects } = state;
 
     return {
-
-        students
+        students, subjects
     };
 }
 
